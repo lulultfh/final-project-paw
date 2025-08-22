9f138120a2f5699bcc3014bfcdaa30c20e5b5632
lululthfh <punyalulu05@gmail.com>
2025-08-22 07:42:26 +0700

    feat: add method get by id, post, put and delete for product

--- be/routes/product-db.js
+++ be/routes/product-db.js
@@ -9,4 +9,59 @@ router.get("/", (req, res) => {
   });
 });
 
+router.get('/:id', (req, res) => {
+    db.query('SELECT * FROM product WHERE id = ?', [req.params.id], (err, results) => {
+        if (err) return res.status(500).send('Internal Server Error');
+        if (results.length === 0) return res.status(404).send('Produk tidak ditemukan');
+        res.json(results[0]);
+    });
+});
+
+router.post('/', (req, res) => {
+    const { nama, price, desc, image, stok, kategori } = req.body;
+    if (!nama || !price || !desc || !image || !stok || !kategori) {
+        return res.status(400).send('Semua kolom wajib diisi!');
+    }
+
+    const query = 'INSERT INTO product (namaProduct, price, description, image, stok, kategori) VALUES (?, ?, ?, ?, ?, ?)';
+    const values = [nama.trim(), price, desc.trim(), image.trim(), stok, kategori.trim()];
+
+    db.query(query, values, (err, results) => {
+        if (err) {
+            console.error('Database error:', err);
+            return res.status(500).send('Internal Server Error');
+        }
+
+        const newProduct = {
+            id: results.insertId,
+            nama: nama.trim(),
+            price: price,
+            desc: desc.trim(),
+            image: image.trim(),
+            stok: stok,
+            kategori: kategori.trim()
+        };
+
+        res.status(201).json(newProduct);
+    });
+});
+
+router.put('/:id', (req, res) => {
+    const { nama, price, desc, image, stok, kategori } = req.body;
+
+    db.query('UPDATE product SET namaProduct = ?, price = ?, description = ?, image = ?, stok = ?, kategori = ? WHERE id = ?', [nama, price, desc, image, stok, kategori, req.params.id], (err, results) => {
+        if (err) return res.status(500).send('Internal Server Error');
+        if (results.affectedRows === 0) return res.status(404).send('Produk tidak ditemukan');
+        res.json({ id: req.params.id, nama, price, desc, image, stok, kategori });
+    });
+});
+
+router.delete('/:id', (req, res) => {
+    db.query('DELETE FROM product WHERE id = ?', [req.params.id], (err, results) => {
+        if (err) return res.status(500).send('Internal Server Error');
+        if (results.affectedRows === 0) return res.status(404).send('Product tidak ditemukan');
+        res.status(204).send();
+    });
+});
+
 module.exports = router;
