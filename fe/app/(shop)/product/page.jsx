9977389f9730a2ff52dfcf3c48312ae121367d0c
lululthfh <punyalulu05@gmail.com>
2025-09-07 22:07:56 +0700

    feat: add prduct to home page

--- fe/app/(shop)/product/page.jsx
+++ fe/app/(shop)/product/page.jsx
@@ -78,7 +78,7 @@ export default function ProductPage() {
 
         <div className="mt-6 grid grid-cols-1 gap-x-6 gap-y-10 sm:grid-cols-2 lg:grid-cols-4 xl:gap-x-8">
           {products.map((product) => (
-            <div key={product.id} className="group relative bg-[#F8D7D1] p-4 rounded-lg shadow-md">
+            <div key={product.id} className="group relative bg-[#F8D7D1] p-4 rounded-lg shadow-md  font-bold">
               <img
                 alt={product.namaProduct}
                 src={product.image}
--- fe/app/page.js
+++ fe/app/page.js
@@ -1,13 +1,15 @@
 // import Navbar from "../components/navbar";
 import SidebarLayout from "@/components/cust/sidebar";
 import Carousel from "@/components/cust/ui/carousel";
+import ProductPage from "./(shop)/product/page";
 
 export default function HomePage() {
   return (
     <div style={{ display: "flex" }}>
       <SidebarLayout />
-      <div className="flex-1 flex justify-center items-start p-6">
+      <div className="flex-1 flex flex-col justify-start items-center p-6">
         <Carousel />
+        <ProductPage />
       </div>
     </div>
   );
